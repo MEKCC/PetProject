3d657a086244c988c2e45a3f940e2d56fdb55314
MaksymPetrychukDigis <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>
2022-04-11 21:40:52 +0300

    added log4j with saving into the file

--- src/main/java/com/petproject/controller/UserController.java
+++ src/main/java/com/petproject/controller/UserController.java
@@ -1,8 +1,9 @@
 package com.petproject.controller;
 
 import com.petproject.model.User;
-import com.petproject.repo.UserRepository;
 import com.petproject.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,11 +15,14 @@ import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 @RestController
 public class UserController {
 
+    private final Logger log = LoggerFactory.getLogger(UserController.class);
+
     @Autowired
     private UserService userService;
 
     @GetMapping(value = "/users", produces = APPLICATION_JSON_VALUE)
     public List<User> getAuthors() {
+        log.info("@@@ <--- !!! SOME INFO !!! ---> @@@");
         return userService.getAllUsers();
     }
 }
