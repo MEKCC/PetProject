aa9b4327f20697931d36b48999666397a3fc715b
MaksymJava-DIGIS <m.petrychuk@do-it.co>
2023-06-30 15:51:48 +0300

    added admin data into DB

---
+++ src/main/resources/db/migration/V4__add_admin_data_into_user_and_roles_tables.sql
@@ -0,0 +1,5 @@
+INSERT INTO testDB.user (username, password, email, firstname, lastname, role_id, is_active)
+VALUES ('MEKCC', 12345678, 'maks@gmail.com', 'Maksym', 'Surname', 1, 1);
+
+INSERT INTO testDB.roles (rolename, role_display_name, `order`)
+VALUES ('Administrator', 'Administrator', 1);
