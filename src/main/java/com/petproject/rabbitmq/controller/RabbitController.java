9d2ed11e2491f88eb0f81e8891efd0385c0c5ddf
MaksymJava-DIGIS <m.petrychuk@do-it.co>
2023-10-02 20:02:14 +0300

    configured prometheus and grafana

--- src/main/java/com/petproject/rabbitmq/controller/RabbitController.java
+++ src/main/java/com/petproject/rabbitmq/controller/RabbitController.java
@@ -2,6 +2,7 @@ package com.petproject.rabbitmq.controller;
 
 import com.petproject.rabbitmq.producer.RabbitMQProducerService;
 import com.petproject.repo.UserRepository;
+import io.micrometer.core.instrument.MeterRegistry;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,10 +13,17 @@ public class RabbitController {
 
     private final RabbitMQProducerService rabbitMQProducerService;
     private final UserRepository userRepository;
+    private final MeterRegistry meterRegistry;
 
     @GetMapping("/send")
     public void sendMessageToRabbit() {
         try {
+            // Отримайте об'єкт для метрики HTTP-запитів
+            var httpRequestsCounter = meterRegistry.counter("http.server.request", "endpoint", "/send");
+
+            // Збільшуйте лічильник для кожного HTTP-запиту
+            httpRequestsCounter.increment();
+
             rabbitMQProducerService.sendMessage(userRepository.findAllUsers(), "september-key");
 
             // Генеруємо приклад помилки
