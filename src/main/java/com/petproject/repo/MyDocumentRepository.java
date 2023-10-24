4afdaafa0b32dd497a70ee3cb85fc3ee2b7c9a7b
MaksymJava-DIGIS <m.petrychuk@do-it.co>
2023-10-24 20:24:51 +0300

    added elasticsearch, kibana

---
+++ src/main/java/com/petproject/repo/MyDocumentRepository.java
@@ -0,0 +1,8 @@
+package com.petproject.repo;
+
+import com.petproject.model.MyDocument;
+import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+
+public interface MyDocumentRepository extends ElasticsearchRepository<MyDocument, String> {
+
+}
